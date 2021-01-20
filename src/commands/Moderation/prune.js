c28559149321935ed2669bb98773ce28b4aee3d2
1GPEX <1GPEX>
2021-01-20 10:35:08 +0700

    Delete Muted

--- src/commands/Moderation/prune.js
+++ src/commands/Moderation/prune.js
@@ -7,6 +7,7 @@ module.exports = class extends Command {
       permissionLevel: 6,
       requiredPermissions: ['MANAGE_MESSAGES'],
       runIn: ['text'],
+      aliases: ['purge'],
       description: 'Prunes a certain amount of messages w/o filter.',
       usage: '[limit:integer] [link|invite|bots|you|me|upload|user:user]',
       usageDelim: ' '
@@ -22,7 +23,7 @@ module.exports = class extends Command {
     }
     messages = messages.array().slice(0, limit)
     await msg.channel.bulkDelete(messages)
-    return msg.sendMessage(`Successfully deleted ${messages.length} messages from ${limit}.`)
+    return msg.sendMessage(`Successfully deleted ${messages.length} messages from ${limit}.`).then(msg => msg.delete())
   }
 
   getFilter (msg, filter, user) {
