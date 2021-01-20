4f34985cfffbf0b161e695b3f48f6078dccd4032
1GPEX <1GPEX>
2021-01-20 10:37:32 +0700

    Fix Schema

--- src/structures/defaultSchema.js
+++ src/structures/defaultSchema.js
@@ -2,7 +2,5 @@ const { Client } = require('klasa')
 
 module.exports = () => {
   Client.defaultGuildSchema.add('minAccAge', 'integer', { default: 1800000 })
-  Client.defaultGuildSchema.add('roles', schema => schema
-    .add('muted', 'role'))
   Client.defaultUserSchema.add('TODOs', 'any', { array: true })
 }
